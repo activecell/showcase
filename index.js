26751daab1211e60467504ef3f470a157c9367ce
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-04-06 17:12:49 +0600

    Added path helper

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 require('coffee-script');
-module.exports = require('./lib');
+exports.app = require('./lib');
+
+// Helper for easy generation valid brunch regexps
+exports.path = function() {
+  var paths = 1 <= arguments.length ? [].slice.call(arguments, 0) : [];
+  var escapedFiles = paths.map(function(pattern) {
+    return pattern.replace(/[\-\[\]\/\{\}\(\)\+\?\.\\\^\$\|]/g, "\\$&");
+  }).join('|');
+  return new RegExp('^' + escapedFiles);
+};

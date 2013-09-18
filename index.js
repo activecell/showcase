fdd0ae9460f7f4528a0a770801bf81663b8248b3
Aluman <alex.reznichenko.dunice@gmail.com>
2013-09-18 19:09:26 +0400

    Updates

--- index.js
+++
@@ -1,8 +0,0 @@
-require('coffee-script');
-exports.app           = require('./lib');
-exports.casper        = require('./lib/casper');
-exports.docco         = require('./lib/docco');
-exports.path          = require('./lib/brunch').path;
-exports.defaultConfig = require('./lib/brunch').defaultConfig;
-exports.isAuth        = require('./lib/is_auth');
-exports.getSections   = require('./lib/kss');
